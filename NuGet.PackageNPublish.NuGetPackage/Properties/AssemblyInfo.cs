41febc28334fe71690d2eda99c7d8171ffca7211
Rammesses <joel@hammond-turner.org.uk>
2012-08-13 22:58:31 +0100

    Merged zip-on-build & added targets packaging project.
    Merged in the zip-on-build code from @JohnStov, and added a NuGet
    packaging project to allow the NuGet.PackageAndPublish.targets file to
    be updated via a NuGet package!

---
+++ NuGet.PackageNPublish.NuGetPackage/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.PackageNPublish")]
+[assembly: AssemblyDescription("NuGet.PackageNPublish MSBuild targets file packaged as a NuGet package!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NuGet.PackageNPublish")]
+[assembly: AssemblyCopyright("Copyright © Landmark UK / Joel Hammond-Turner 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
