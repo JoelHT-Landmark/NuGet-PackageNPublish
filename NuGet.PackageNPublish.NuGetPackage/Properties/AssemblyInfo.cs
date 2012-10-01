0fc3faa248e8d3a0b39728bbaa595a08d401ffe1
Joel Hammond-Turner <joel.hammond-turner@landmark.co.uk>
2012-10-01 08:54:07 +0100

    v0.6.0 - Updated to vs2012 tooling
    The Extension project has now been updated to build under VS2012 only,
    targetting VS2010-12 Pro and Ultimate (until I can work out what the
    product codes for Express are).

--- NuGet.PackageNPublish.NuGetPackage/Properties/AssemblyInfo.cs
+++ NuGet.PackageNPublish.NuGetPackage/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.5.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
